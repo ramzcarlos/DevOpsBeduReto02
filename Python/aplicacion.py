ee2f5634a99f4821c71f5a497a3610187272d524
Carlos Ramirez <carlos.ramz.pina@gmail.com>
2022-11-17 17:11:11 -0600

    crea nueva clase

--- Python/aplicacion.py
+++ Python/aplicacion.py
@@ -1,7 +1,12 @@
+from muestra import *
+class Aplicacion():
 
-def main():
-	# Imprimir en pantalla mensaje.
-	print ("Hola Bedu! Esto Es Codigo en la Rama Principal")
-	#llamando a funcion principal.
-	
-main()
+	def main(self):
+		# Imprimir en pantalla mensaje.
+		print ("Hola Bedu! Esto Es Codigo en la Rama Principal")
+		#llamando a funcion principal.
+		inicioMuestra=Muestra()
+		inicioMuestra.main()
+
+inicio=Aplicacion()	
+inicio.main()
