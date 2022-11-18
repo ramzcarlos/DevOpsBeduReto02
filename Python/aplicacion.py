b11d6446da410eb5813f108b73dd0b69fae63eb3
Carlos Ramirez <carlos.ramz.pina@gmail.com>
2022-11-18 14:20:43 -0600

    codigo del desarrollador 3

--- Python/aplicacion.py
+++ Python/aplicacion.py
@@ -1,12 +1,12 @@
-from muestra import *
+from BotonAtras import *
 class Aplicacion():
 
 	def main(self):
 		# Imprimir en pantalla mensaje.
 		print ("Hola Bedu! Esto Es Codigo en la Rama Principal")
 		#llamando a funcion principal.
-		inicioMuestra=Muestra()
-		inicioMuestra.main()
+		inicioBotonAtras=BotonAtras()
+		inicioBotonAtras.main()
 
 inicio=Aplicacion()	
 inicio.main()
